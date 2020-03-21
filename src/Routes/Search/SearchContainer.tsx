51ccc709c0850aa11fa38f687be140ea6eb7e5c2
seungho Jeon <urijsh612@gmail.com>
2020-03-21 17:44:19 +0900

    [수정] search 페이지 query-string 사용

--- src/Routes/Search/SearchContainer.tsx
+++ src/Routes/Search/SearchContainer.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import queryString from "query-string";
 
 const SearchContainer: React.FC = () => {
   const location = useLocation();
-  const term = decodeURI(location.search.split("=")[1]);
+  const { term } = queryString.parse(location.search);
 
   return <div>{term}</div>;
 };
