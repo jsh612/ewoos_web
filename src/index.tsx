8ac1d8100acd6fc88a7ee85e876fed04d529f090
seungho Jeon <urijsh612@gmail.com>
2020-03-31 23:35:30 +0900

    [오류] globalStyles 배포오류

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./Components/App";
 import { ApolloProvider } from "@apollo/react-hooks";
 import client from "./Apollo/apollo";
-import GrobalStyles from "./Styles/globalStyles";
 import "antd/dist/antd.css";
 import MainProvider from "./Components/MainContext";
-
+import GrobalStyles from "../src/Styles/globalStyles";
 ReactDOM.render(
   <ApolloProvider client={client}>
     <GrobalStyles />
