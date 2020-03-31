42a9d698c32ad3d4fee814353da25055c9a17f00
seungho Jeon <urijsh612@gmail.com>
2020-03-31 23:23:15 +0900

    [수정] global styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./Components/App";
 import { ApolloProvider } from "@apollo/react-hooks";
 import client from "./Apollo/apollo";
-import GrobalStyles from "./Styles/global_styles";
+import GrobalStyles from "./Styles/globalStyles";
 import "antd/dist/antd.css";
 import MainProvider from "./Components/MainContext";
 
